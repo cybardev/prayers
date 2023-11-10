7ea1fe666294e174a36a80cb9660892919b876e5
Sheikh <sheikh@cybar.dev>
2023-11-10 19:19:44 -0400

    refactor: remove direct dependency on PrayTimes.js

--- public/assets/script.js
+++ public/assets/script.js
@@ -2,59 +2,13 @@
 const $$ = (el) => document.querySelector(el);
 
 function parseURL(url) {
-    const args = new URLSearchParams(url);
-    return {
-        year: parseInt(args.get("year")),
-        month: parseInt(args.get("month")),
-        date: parseInt(args.get("date")),
-        latitude: parseFloat(args.get("latitude")),
-        longitude: parseFloat(args.get("longitude")),
-    };
-}
-
-function processDate(year, month, date) {
-    // default date: today
-    const today = new Date();
-    return {
-        year: isNaN(year) ? today.getFullYear() : year,
-        month: isNaN(month) ? today.getMonth() + 1 : month,
-        date: isNaN(date) ? today.getDate() : date,
-    };
-}
-
-function processPos(latitude, longitude) {
-    // default coordinates: Halifax, NS, Canada
-    const defaultPos = { latitude: 44.65, longitude: -63.57 };
-    return {
-        latitude: isNaN(latitude) ? defaultPos.latitude : latitude,
-        longitude: isNaN(longitude) ? defaultPos.longitude : longitude,
-    };
+    return new URL(url).search;
 }
 
 function getPrayerTimes(params) {
-    const date = processDate(params.year, params.month, params.date);
-    const pos = processPos(params.latitude, params.longitude);
-    prayTimes.setMethod("ISNA");
-    const times = prayTimes.getTimes(
-        [date.year, date.month, date.date],
-        [pos.latitude, pos.longitude],
-        "auto",
-        "auto",
-        "12h"
-    );
-    return {
-        meta: {
-            date: date,
-            position: pos,
-        },
-        data: {
-            fajr: times.fajr,
-            zuhr: times.dhuhr,
-            asr: times.asr,
-            maghrib: times.maghrib,
-            isha: times.isha,
-        },
-    };
+    const res = null;
+    // TODO: fetch prayer times from API
+    return res;
 }
 
 function apiSite() {
