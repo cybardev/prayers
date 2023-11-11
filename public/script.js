50e2d4d772e4dfd598f6e4f65c9c3c47fec842d3
Sheikh <sheikh@cybar.dev>
2023-11-10 20:53:21 -0400

    fix: use proper callback function to fetch data from API

--- public/script.js
+++ public/script.js
@@ -1,24 +1,19 @@
 // alias for retrieving DOM elements
 const $$ = (el) => document.querySelector(el);
 
-function getPrayerTimes(params) {
-    let result = null;
-    fetch(`https://prayertimes.cybar.dev/${params}`)
-        .then((res) => res.json())
-        .then((data) => (result = data))
+function getPrayerTimes() {
+    fetch(`https://prayertimes.cybar.dev/${window.location.search}`)
+        .then((response) => response.json())
+        .then((body) => {
+            for (const prayer in body.data) {
+                $$(`#prayer-${prayer}`).innerText =
+                    prayer[0].toUpperCase() + prayer.slice(1);
+                $$(`#time-${prayer}`).innerText =
+                    body.data[prayer].toUpperCase();
+                $$("#latitude").innerText = body.meta.position.latitude;
+                $$("#longitude").innerText = body.meta.position.longitude;
+                $$("#date").innerText = Object.values(body.meta.date).join("-");
+            }
+        })
         .catch((err) => console.log(err));
-    return result;
-}
-
-function userSite() {
-    const prayerTimes = getPrayerTimes(window.location.search);
-    for (const prayer in prayerTimes.data) {
-        $$(`#prayer-${prayer}`).innerText =
-            prayer[0].toUpperCase() + prayer.slice(1);
-        $$(`#time-${prayer}`).innerText =
-            prayerTimes.data[prayer].toUpperCase();
-        $$("#latitude").innerText = prayerTimes.meta.position.latitude;
-        $$("#longitude").innerText = prayerTimes.meta.position.longitude;
-        $$("#date").innerText = Object.values(prayerTimes.meta.date).join("-");
-    }
 }
