87f8d1afffff574237e37503034eeadddac50270
Sheikh <sheikh@cybar.dev>
2023-11-11 10:34:06 -0400

    refactor: dynamically generate prayer entries from API response

--- public/script.js
+++ public/script.js
@@ -1,6 +1,10 @@
 // alias for retrieving DOM elements
 const $$ = (el) => document.querySelector(el);
 
+function prayerEntry(name, time) {
+    return `<tr><td>${name}</td><td>${time}</td></tr>`;
+}
+
 function getPrayerTimes() {
     fetch(`https://prayertimes.cybar.dev/${window.location.search}`)
         .then((response) => response.json())
@@ -10,9 +14,9 @@ function getPrayerTimes() {
             $$("#latitude").innerText = body.meta.position.latitude;
             $$("#longitude").innerText = body.meta.position.longitude;
             // prayer times
+            const prayers = $$("#prayers");
             for (const prayer in body.data) {
-                $$(`#prayer-${prayer}`).innerText = prayer;
-                $$(`#time-${prayer}`).innerText = body.data[prayer];
+                prayers.innerHTML += prayerEntry(prayer, body.data[prayer]);
             }
         })
         .catch((err) => console.error(err));
