f5440fabe9f581a4ac03d1356fb8ffd9374da27b
Sheikh <sheikh@cybar.dev>
2023-11-10 23:14:54 -0400

    refactor: avoid unnecessary loop items

--- public/script.js
+++ public/script.js
@@ -5,15 +5,17 @@ function getPrayerTimes() {
     fetch(`https://prayertimes.cybar.dev/${window.location.search}`)
         .then((response) => response.json())
         .then((body) => {
+            // metadata
+            $$("#date").innerText = Object.values(body.meta.date).join("-");
+            $$("#latitude").innerText = body.meta.position.latitude;
+            $$("#longitude").innerText = body.meta.position.longitude;
+            // prayer times
             for (const prayer in body.data) {
                 $$(`#prayer-${prayer}`).innerText =
                     prayer[0].toUpperCase() + prayer.slice(1);
                 $$(`#time-${prayer}`).innerText =
                     body.data[prayer].toUpperCase();
-                $$("#latitude").innerText = body.meta.position.latitude;
-                $$("#longitude").innerText = body.meta.position.longitude;
-                $$("#date").innerText = Object.values(body.meta.date).join("-");
             }
         })
-        .catch((err) => console.log(err));
+        .catch((err) => console.error(err));
 }
