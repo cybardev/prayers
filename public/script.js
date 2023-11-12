a7b272af1922633da06c39677f09e0d817207895
Sheikh <sheikh@cybar.dev>
2023-11-12 09:54:43 -0400

    refactor: improve clarity using named callbacks

--- public/script.js
+++ public/script.js
@@ -5,22 +5,21 @@ function prayerEntry(name, time) {
     return `<tr><td>${name}</td><td>${time}</td></tr>`;
 }
 
+function populatePrayerEntries(entries) {
+    // metadata
+    $$("#date").innerText = Object.values(entries.meta.date).join("-");
+    $$("#latitude").innerText = entries.meta.position.latitude;
+    $$("#longitude").innerText = entries.meta.position.longitude;
+    // prayer times
+    const prayers = $$("#prayers");
+    for (const [prayer, time] of Object.entries(entries.data.prayers)) {
+        prayers.insertAdjacentHTML("beforeend", prayerEntry(prayer, time));
+    }
+}
+
 function getPrayerTimes() {
     fetch(`/api/${window.location.search}`)
         .then((response) => response.json())
-        .then((body) => {
-            // metadata
-            $$("#date").innerText = Object.values(body.meta.date).join("-");
-            $$("#latitude").innerText = body.meta.position.latitude;
-            $$("#longitude").innerText = body.meta.position.longitude;
-            // prayer times
-            const prayers = $$("#prayers");
-            for (const [prayer, time] of Object.entries(body.data.prayers)) {
-                prayers.insertAdjacentHTML(
-                    "beforeend",
-                    prayerEntry(prayer, time)
-                );
-            }
-        })
-        .catch((err) => console.error(err));
+        .then(populatePrayerEntries)
+        .catch(console.error);
 }
