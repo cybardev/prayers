2202fdf79e526f888d6f51bc45f1b16c46255dbd
Sheikh <sheikh@cybar.dev>
2023-11-12 10:53:39 -0400

    refactor: simplify code using semantic function parameters

--- public/script.js
+++ public/script.js
@@ -5,14 +5,14 @@ function prayerEntry(name, time) {
     return `<tr><td>${name}</td><td>${time}</td></tr>`;
 }
 
-function populatePrayerEntries(entries) {
+function populatePrayerEntries(meta, data) {
     // metadata
-    $$("#date").innerText = Object.values(entries.meta.date).join("-");
-    $$("#latitude").innerText = entries.meta.position.latitude;
-    $$("#longitude").innerText = entries.meta.position.longitude;
+    $$("#date").innerText = Object.values(meta.date).join("-");
+    $$("#latitude").innerText = meta.position.latitude;
+    $$("#longitude").innerText = meta.position.longitude;
     // prayer times
     const prayers = $$("#prayers");
-    for (const [prayer, time] of Object.entries(entries.data.prayers)) {
+    for (const [prayer, time] of Object.entries(data.prayers)) {
         prayers.insertAdjacentHTML("beforeend", prayerEntry(prayer, time));
     }
 }
@@ -20,6 +20,6 @@ function populatePrayerEntries(entries) {
 function getPrayerTimes() {
     fetch(`/api/${window.location.search}`)
         .then((response) => response.json())
-        .then(populatePrayerEntries)
-        .catch(console.error);
+        .then((body) => populatePrayerEntries(body.meta, body.data))
+        .catch((err) => console.error(err));
 }
