d8eb0f528b59037dda413a8a7e65eca372d40088
Sheikh <sheikh@cybar.dev>
2023-11-14 18:43:40 -0400

    fix: add missing context

--- functions/api/index.js
+++ functions/api/index.js
@@ -2,6 +2,6 @@ import res from "../../src/prayertimes.mjs";
 
 export function onRequest(context) {
     return context.request.method === "GET"
-        ? res.validResponse(request.url)
+        ? res.validResponse(context.request.url)
         : res.invalidResponse();
 }
