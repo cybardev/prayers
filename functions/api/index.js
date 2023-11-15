d1d1edce08a91b8e1067fe309efa502db9c9046a
Sheikh <sheikh@cybar.dev>
2023-11-14 23:05:52 -0400

    refactor: rename main API module file

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,4 +1,4 @@
-import successResponse from "../../src/prayertimes.mjs";
+import successResponse from "../../src/prayers.mjs";
 
 export function onRequestGet(context) {
     return successResponse(context.request.url);
